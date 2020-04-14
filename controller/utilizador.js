e69325799038bb832655e77dc1796bdd64f04346
maneqas <55759115+maneqas@users.noreply.github.com>
2020-04-14 15:56:25 +0100

    Divisão do controller utilizador.js

--- controller/utilizador.js
+++ controller/utilizador.js
@@ -1,56 +1,5 @@
 $(document).ready(function () {
-    getUtilizador();
     dadosUtilizador();
-});
-
-//Carregar o registo de um novo utilzador
-('#formNewRegisto').validator().on('submit', function (e) {
-
-    if (e.isDefaultPrevented()) {
-        alert("Formulário com erros")
-    }
-    else {
-        event.preventDefault();
-
-        var data = {};
-        data.profissao = $('#profissao').val();
-        data.ramo_emp = $('#ramoEp').val();
-        data.num_trabalhadores = $('#nEmpregados').val();
-        data.regiao_pais = $('#regiao').val();
-        data.area_cientifica = $('#area').val();
-        data.ciclo_estudo = $('#estudo').val();
-        data.nome = $('#nome').val();
-        data.idade = $('#idade').val();
-        data.genero = $('#genero').val();
-        data.email = $('#email').val();
-        data.password = $('#password').val();
-        data.descricao = $('#descricao').val();
-        data.perfil = 'Perfil público';
-
-        console.log(data);
-        $('#formNewRegisto')[0].reset();
-
-        $.ajax({
-            type: 'POST',
-            url: '/saveUtilizador',
-            data: data,
-            success: function (result) {
-                if (result.status == 200) {
-                    alert("Bem Vindo à HeplMe Projects")
-                    window.location.assign("/forum");
-                }
-                else {
-                    alert("O seu Registo não foi efetuado, por favor tente outra vez");
-                }
-            },
-            error: function (xhr, textStatus, errorThrown) {
-                console.log(xhr.responseText);
-                console.log(textStatus);
-                console.log(errorThrown);
-                alert("erro");
-            }
-        });
-    }
 });
 
 //Carregar dados para a página perfil utilziador
@@ -93,7 +42,7 @@ function dadosUtilizador() {
     });
 };
 
-//Carregar os dados atualziados pelo utilizador
+//Carregar os dados atualizados pelo utilizador
 $('#formUtilizador').validator().on('submit', function (e) {
 
     if (e.isDefaultPrevented()) {
@@ -117,18 +66,21 @@ $('#formUtilizador').validator().on('submit', function (e) {
         data.descricao = $('#descricao').val();
 
         console.log(data);
-        $("#formUtilziador")[0].reset();
 
         $.ajax({
             type: 'POST',
-            url: '/',
+            url: '/saveUtilizador',
             data: JSON.stringify(data),
             contentType: 'application/json',
             success: function (result) {
                 if (result.status == 200) {
                     alert("submetido com sucesso");
+                    $("#formUtilziador")[0].reset();
+                    dadosUtilizador();
                 }
-                dadosUtilizador();
+                else {
+                    console.log("Erro");
+                }
             },
             error: function (xhr, textStatus, errorThrown) {
                 console.log(xhr.responseText);
@@ -139,48 +91,3 @@ $('#formUtilizador').validator().on('submit', function (e) {
         });
     }
 });
-
-//Carregar dados para a página utilizador-admin
-function getUtilizador() {
-    var data = {};
-
-    $.ajax({
-        type: "GET",
-        url: '/readUtilizador',
-        data: JSON.stringify(data),
-        contentType: 'application/json; charset=utf-8',
-        
-
-        success: function (data, status, request) {
-            console.log(data);
-            if (request.status == 200) {
-                var txt = "";
-            
-                txt += '<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" >';
-                txt += "<thead>";
-                txt += "<tr><th>Nome</th><th>Idade</th><th>Género</th><th>Profissão</th><th>Ramo Empresarial</th><th>Nº Empregados</th><th>Região do País</th><th>Área de Conhecimento</th><th>Ciclo de Estudo</th><th>Email</th><th>Password</th><th>Descrição</th></tr></thead><tbody>";
-
-                data.forEach(function (row) {
-                    txt += "<tr><td>" + row.nome + "</td><td>" + row.idade + "</td><td>" + row.genero + "</td><td>" + row.profissao +
-                        "</td><td>" + row.ramo_emp + "</td><td>" + row.num_trabalhadores + "</td><td>" + row.regiao_pais + "</td><td>" + row.area_cientifica +
-                        "</td><td>" + row.ciclo_estudo + "</td><td>" + row.email + "</td><td>" + row.password + "</td><td>" + row.descricao + "</td></tr>";
-
-                });
-                txt += "</tbody></table>";
-
-                $("#tabela_utilizador").html(txt);
-            }
-            else {
-                console.log("Erro");
-            }
-        },
-        error: function (xhr, textStatus, errorThrown) {
-            console.log(xhr.responseText);
-            console.log(textStatus);
-            console.log(errorThrown);
-
-        }
-
-    });
-
-};
