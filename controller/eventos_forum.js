08314f50696c39f8c9fa0fdd14fa1833bbfb151c
Pintas <51865970+Pintas@users.noreply.github.com>
2020-05-21 15:31:31 +0100

    meh

--- controller/eventos_forum.js
+++ controller/eventos_forum.js
@@ -48,7 +48,7 @@ function seeEvento(id) {
             result.forEach(function(row) {
                 txt += "<img alt=\"\" style=\"max-height:360px; max-width:360px;\" src=\"../../../uploads/" + row.imagem + "\">";
                 $("#modalImg").html(txt);
-                $("#nomeEvento").html(row.nomeEvento);
+                $("#nomeEvento").html(row.nomeEvent o);
                 $("#descricaoEvento").html(row.tipoEvento);
             });
         },
